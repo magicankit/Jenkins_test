1ee9250755f4eede7f75203cc36cc4a706707030
Ankit Arora <ankit1.arora@infogain.com>
2017-04-13 16:00:36 +0530

    Jenkins Project added

---
+++ Week1_Assignment/src/main/java/com/infogain/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.infogain;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to infogain!");
+		// TODO Auto-generated method stub
+
+	}
+
+}
